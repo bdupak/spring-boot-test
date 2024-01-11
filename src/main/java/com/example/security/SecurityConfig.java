5d6105b16db5354943afbe98ae6f6b0141e9ca6c
Bohdan_Dupak <bohdan_dupak@epam.com>
2024-01-11 13:25:23 +0200

    added support with spring security

---
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.example.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Value("${spring.security.user.name}")
+  private String username;
+  @Value("${spring.security.user.password}")
+  private String password;
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService(final PasswordEncoder passwordEncoder) {
+    UserDetails user = User.withUsername(username)
+        .password(passwordEncoder.encode(password))
+        .roles("SUPER-USER")
+        .build();
+
+    return new InMemoryUserDetailsManager(user);
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .httpBasic();
+    return http.build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }
+}
