d885c2290c0b25ad1744166f696f3006081de234
Bohdan_Dupak <bohdan_dupak@epam.com>
2024-01-16 14:12:34 +0200

    added additional logs

--- src/main/java/com/example/aspect/LoggingAspect.java
+++ src/main/java/com/example/aspect/LoggingAspect.java
@@ -11,25 +11,45 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect
 public class LoggingAspect {
-  private Logger logger;
-  private String className;
-  private String methodName;
+  private static final String LOG_BEFORE_CALL_METHOD = "Start method: ";
+  private static final String LOG_AFTER_CALL_METHOD = "Finish method: ";
 
-//  @Before(value = "com.example.aspect.Pointcuts.allMethods()")
+  //  @Before(value = "com.example.aspect.Pointcuts.allMethods()")
   @Before(value = "execution(* com.example.api..*(..))")
   public void logBeforeMethod(JoinPoint joinPoint) {
-    className = joinPoint.getTarget().getClass().getName();
-    logger = LoggerFactory.getLogger(className);
-    methodName = joinPoint.getSignature().getName();
-    logger.info("Start method: " + methodName);
+    commonLog(joinPoint, LOG_BEFORE_CALL_METHOD);
   }
 
 //  @After(value = "com.example.aspect.Pointcuts.allMethods()")
   @After(value = "execution(* com.example.api..*(..))")
   public void logAfterMethod(JoinPoint joinPoint) {
-    className = joinPoint.getTarget().getClass().getName();
-    logger = LoggerFactory.getLogger(className);
-    methodName = joinPoint.getSignature().getName();
-    logger.info("Finish method: " + methodName);
+    commonLog(joinPoint, LOG_AFTER_CALL_METHOD);
+  }
+
+  private void commonLog(JoinPoint joinPoint, String message) {
+    final String className = joinPoint.getTarget().getClass().getName();
+    final Logger logger = LoggerFactory.getLogger(className);
+    final String methodName = joinPoint.getSignature().getName();
+    logger.info(message + methodName);
+  }
+
+  @Before(value = "execution(* com.example.database..*(..))")
+  public void logBeforeDatabaseMethod(JoinPoint joinPoint) {
+    commonLog(joinPoint, LOG_BEFORE_CALL_METHOD);
+  }
+
+  @After(value = "execution(* com.example.database..*(..))")
+  public void logAfterDatabaseMethod(JoinPoint joinPoint) {
+    commonLog(joinPoint, LOG_AFTER_CALL_METHOD);
+  }
+
+  @Before(value = "execution(* com.example.mapper..*(..))")
+  public void logBeforeMapperMethod(JoinPoint joinPoint) {
+    commonLog(joinPoint, LOG_BEFORE_CALL_METHOD);
+  }
+
+  @After(value = "execution(* com.example.mapper..*(..))")
+  public void logAfterMapperMethod(JoinPoint joinPoint) {
+    commonLog(joinPoint, LOG_AFTER_CALL_METHOD);
   }
 }
